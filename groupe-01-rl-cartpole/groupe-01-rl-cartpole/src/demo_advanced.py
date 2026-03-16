e84c7cb59b6876f0138f0ea20f7f76d5cb01087b
Aya Asseli <aya.asseli@edu.ece.fr>
2026-03-16 12:23:41 +0100

    add SAC training for Pendulum

---
+++ groupe-01-rl-cartpole/groupe-01-rl-cartpole/src/demo_advanced.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 12:21:45 2026
+
+@author: assel
+"""
+
---
+++ groupe-01-rl-cartpole/groupe-01-rl-cartpole/src/train_sac.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 12:21:00 2026
+
+@author: assel
+"""
+
